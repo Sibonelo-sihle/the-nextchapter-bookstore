b39dc477f9b49ace81604fa62e8d3fc8f6a34987
Yanga Masibulele Mdede <yanga.mdede@capaciti.org.za>
2026-05-04 14:00:26 +0200

    new

--- send-eft-order.php
+++ send-eft-order.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     Bank: Capitec
     Account Name: Mr Buthelezi
-    Account Number: XXXXXXXX
+    Account Number: XXXXXXXXXX
     Branch Code: 250655
 
     Send proof of payment via WhatsApp.
